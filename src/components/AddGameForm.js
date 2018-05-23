f77e22af3ecbdfc26714618c555b3b8e6ddd7470
Ryan Forgie <ryan.j.forgie@gmail.com>
2018-05-23 08:30:34 +0100

    add ratingCalculator

--- src/components/AddGameForm.js
+++ src/components/AddGameForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Input } from 'semantic-ui-react'
+import { Button } from 'semantic-ui-react'
 import withAuthorization from './withAuthorization';
 import { connect } from 'react-redux';
 import { compose } from 'recompose';
